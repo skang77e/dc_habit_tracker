93e755c929dc06ed65e24926d199a65d4615ebd3
Jon Kang <jk2060@georgetown.edu>
2022-01-02 21:35:31 -0500

    finish habit tracker

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React, { memo } from "react";
+
+const Navbar = memo((props) => {
+  return (
+    <div className='navbar'>
+      <i className='navbar-logo fas fa-leaf'></i>
+      <span>Habit Tracker</span>
+      <span className='navbar-count'>{props.totalCount}</span>
+    </div>
+  );
+});
+export default Navbar;
